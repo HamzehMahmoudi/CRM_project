d2b122c1d4e6b227aae11a25782cbe5dac9439d4
Hamzeh <hamzehmahmoudi7@gmail.com>
2021-08-09 01:05:27 +0430

    delete register view

--- users/views.py
+++ users/views.py
@@ -1,17 +1,4 @@
-from django.shortcuts import render
-from .forms import UserRegisterForm
 from django.views import generic
-from django.urls import reverse_lazy
-
-
-# login and logout views represented using django views
-class Register(generic.CreateView):
-    """
-    registeter a user to login 
-    """
-    template_name = 'users/register.html'
-    success_url = reverse_lazy('login')
-    form_class = UserRegisterForm
 
 
 class Index(generic.TemplateView):
